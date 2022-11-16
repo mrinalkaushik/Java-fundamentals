3b38d478f331352c9c7fb6062f6657085c14bb36
mrinal kaushik <mrinalrocks24x7gmail.com>
2022-11-16 16:46:05 +0530

    ADD: check two strings are equal or not.

---
+++ Cmpstring.java
@@ -0,0 +1,15 @@
+package com.bridgelab.day1;
+
+public class cmpstring {
+
+	public static void main(String[] args) {
+		String statement1=" my name is mrinal ";
+        String statement2="my name is mrinal ";
+
+       if (statement1==statement2);
+       { System.out.println("are common string");
+        }
+
+	}
+
+}
