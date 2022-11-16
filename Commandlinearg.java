3f41c2c25bf3061301dd3d99f3b66e2c939c40d3
mrinal kaushik <mrinalrocks24x7gmail.com>
2022-11-16 16:58:23 +0530

    ADD:sum of command-line arguments

---
+++ Commandlinearg.java
@@ -0,0 +1,12 @@
+package com.bridgelab.day1;
+
+public class commandlinearg {
+	
+public static void main(String[] args) 
+{System.out.println("This is a simple command line program!");
+    System.out.println("Your Command Line arguments are:");  
+    for (String str: args) {
+      System.out.println(str);
+}
+}
+}
